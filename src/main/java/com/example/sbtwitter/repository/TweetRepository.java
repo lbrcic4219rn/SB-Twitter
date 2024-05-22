99caef2f0a89d1f2741a1615fbd21e20cb2e6585
luka.brcic <lbrcic4219rn@raf.edu.rs>
2024-05-22 09:22:33 +0200

    Fix

--- src/main/java/com/example/sbtwitter/repository/TweetRepository.java
+++ src/main/java/com/example/sbtwitter/repository/TweetRepository.java
@@ -1,6 +1,5 @@
 package com.example.sbtwitter.repository;
 
-import com.example.sbtwitter.model.HashTag;
 import com.example.sbtwitter.model.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
