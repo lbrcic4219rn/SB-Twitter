03082642db283729551939680e91ef2bcaa2ae01
luka.brcic <lbrcic4219rn@raf.edu.rs>
2024-05-31 09:11:45 +0200

    N + 1 solution

--- src/main/java/com/example/sbtwitter/repository/TweetRepository.java
+++ src/main/java/com/example/sbtwitter/repository/TweetRepository.java
@@ -1,6 +1,10 @@
 package com.example.sbtwitter.repository;
 
 import com.example.sbtwitter.model.Tweet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -9,4 +13,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * Provides basic CRUD operations and additional query methods.
  */
 public interface TweetRepository extends JpaRepository<Tweet, Long>, JpaSpecificationExecutor<Tweet> {
+    @EntityGraph(attributePaths = {"hashTags"})
+    Page<Tweet> findAll(Specification<Tweet> spec, Pageable pageable);
 }
